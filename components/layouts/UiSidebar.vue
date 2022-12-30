57925c739f1d477bb3ed5e2fdb2ac72c0c0572d4
zuramai <saugi.dev@gmail.com>
2022-12-30 10:53:18 +0700

    feat: sidebar

---
+++ components/layouts/UiSidebar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="sidebar | flex items-center fixed h-screen top-0 w-10 p-2 border-r-1">
+    <ul class="main-menu | px-0 list-none">
+      <layouts-ui-sidebar-item to="/">
+        <template #icon>
+          <div class="i-mdi:home w-6 h-6" />
+        </template>
+      </layouts-ui-sidebar-item>
+      <layouts-ui-sidebar-item to="/race">
+        <template #icon>
+          <div class="i-mdi:flash-outline w-6 h-6" />
+        </template>
+      </layouts-ui-sidebar-item>
+      <layouts-ui-sidebar-item to="/learn">
+        <template #icon>
+          <div class="i-mdi:reminder w-6 h-6" />
+        </template>
+      </layouts-ui-sidebar-item>
+    </ul>
+  </div>
+</template>
---
+++ components/layouts/UiSidebarItem.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+defineProps<{
+  to: string
+}>()
+</script>
+
+<template>
+  <li>
+    <nuxt-link :to="to" class="block p-2">
+      <slot name="icon">
+        <div class="i-mdi:home" />
+      </slot>
+    </nuxt-link>
+  </li>
+</template>
