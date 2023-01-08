2ae55259bbd43e32da9adff079cae3093f0cf99d
zuramai <saugi.dev@gmail.com>
2023-01-08 15:41:20 +0700

    feat: dummy page

---
+++ pages/auth/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page-settings">
+    <div class="container mx-auto px-25">
+      <h1 class="text-center text-5xl">
+        Login
+      </h1>
+
+      <div class="flex justify-center">
+        <UiCard />
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/settings.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="page-settings">
+    <div class="container mx-auto px-25">
+      <h1 class="text-center text-5xl">
+        Settings
+      </h1>
+    </div>
+  </div>
+</template>
